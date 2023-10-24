1d5a765a111aebe8abafd764e2483e190a6dd3bb
OzgurSonmez <ozgursonmez00@gmail.com>
2023-10-24 03:08:21 +0300

    Add

---
+++ HomeWorks_1/01_int_data_type/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number1 = 2147483647;
+int number2 = -2147483648;
+Console.WriteLine("Number1 is {0}",number1);
+Console.WriteLine("Number2 is {0}",number2);

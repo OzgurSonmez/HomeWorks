c998345e96389e163ebd68a66e3f90b5a45aa7c1
OzgurSonmez <ozgursonmez00@gmail.com>
2023-10-24 12:48:05 +0300

    Data Types

--- HomeWorks_1/02_long_data_type/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-long number1 = 9223372036854775807;
-long number2 = -9223372036854775808;
-Console.WriteLine("Number1 is {0}", number1);
-Console.WriteLine("Number2 is {0}", number2);
--- HomeWorks_1/03_short_data_type/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int number1 = 2147483647;
-long number2 = -9223372036854775808;
-short number3 = -32768;
-short number4 = 32767;
-Console.WriteLine("Number1 is {0}", number1);
-Console.WriteLine("Number2 is {0}", number2);
-Console.WriteLine("Number3 is {0}", number3);
-Console.WriteLine("Number4 is {0}", number4);

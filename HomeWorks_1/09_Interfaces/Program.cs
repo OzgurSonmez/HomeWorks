fb2cd774acd4ebcba07b7616217e82f3f0ac067e
OzgurSonmez <ozgursonmez00@gmail.com>
2023-10-25 20:27:01 +0300

    Interfaces

---
+++ HomeWorks_1/09_Interfaces/Program.cs
@@ -0,0 +1,82 @@
+﻿
+using System.Runtime.CompilerServices;
+using _09_Interfaces;
+
+//InterfacesIntro();
+//Demo();
+
+ICustomerDal[] customerDals = new ICustomerDal[3]
+    {
+        new SqlCustomerDal(),
+        new OracleCustomerDal(),
+        new MySQLCustomerDal()
+    };
+foreach (var customerDal in customerDals)
+{
+    customerDal.Add();
+}
+
+static void Demo()
+{
+    CustomerManager customerManager = new CustomerManager();
+    customerManager.Add(new SqlCustomerDal());
+    customerManager.Add(new OracleCustomerDal());
+}
+static void InterfacesIntro()
+{
+    PersonManager manager = new PersonManager();
+    Customer customer = new Customer
+    {
+        Id = 1,
+        FirstName = "Özgür",
+        LastName = "Sönmez",
+        Address = "Pendik"
+    };
+    manager.Add(customer);
+
+    Student student = new Student
+    {
+        Id = 2,
+        FirstName = "Burak",
+        LastName = "Pehlivan",
+        Departmant = "Computer Sciences"
+    };
+
+    manager.Add(student);
+}
+
+interface IPerson
+{
+    int Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+}
+
+class Customer : IPerson
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Address { get; set; }
+}
+
+class Student : IPerson 
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Departmant { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person)
+    {
+        Console.WriteLine(person.FirstName);
+    }
+}
+
+
+

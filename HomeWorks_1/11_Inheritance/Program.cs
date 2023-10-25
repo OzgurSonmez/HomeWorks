b2ff8ca9db7dafeac6f47847fcc23c9af6955540
OzgurSonmez <ozgursonmez00@gmail.com>
2023-10-25 21:36:05 +0300

    Inheritance

---
+++ HomeWorks_1/11_Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Person[] persons = new Person[3]
+    {
+        new Customer{FirstName = "Özgür"},
+        new Student{FirstName = "Burak"},
+        new Person{FirstName = "Çağrı"}
+    };
+foreach (Person person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}

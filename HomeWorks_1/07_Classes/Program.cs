93af98799939f62d1f95a970fcc223ea86e28636
OzgurSonmez <ozgursonmez00@gmail.com>
2023-10-25 19:17:46 +0300

    Classes

---
+++ HomeWorks_1/07_Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using _07_Classes;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Customer customer = new Customer();
+customer.City = "Sinop";
+customer.Id = 1;
+customer.FirstName = "Özgür";
+customer.LastName = "Sönmez";
+
+Customer customer2 = new Customer
+{
+    City = "İstanbul",
+    Id = 2,
+    FirstName = "Burak",
+    LastName = "Pehlivan"
+};
+
+Console.WriteLine(customer.FirstName);
+
+
